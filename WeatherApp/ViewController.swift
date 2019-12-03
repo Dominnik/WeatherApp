c6340da662cfe85fe4654c5713d7b807c830622a
Andrey Kovalenko <dominnik77@gmail.com>
2019-12-03 14:49:43 +0400

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Andrey Kovalenko on 03.12.2019.
+//  Copyright © 2019 Andrey Kovalenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
